0632e13f874d81ecdf39f2d45900e4be4a77975c
Tyler Farris <TylerpFarris@gmail.com>
2022-06-21 11:07:30 -0600

    logs in manager

--- openlineage/airflow/extractors/manager.py
+++ openlineage/airflow/extractors/manager.py
@@ -1,13 +1,21 @@
 import logging
 from typing import Optional, Type
 
-from openlineage.airflow.extractors import TaskMetadata, BaseExtractor, Extractors
-from openlineage.airflow.facets import UnknownOperatorAttributeRunFacet, UnknownOperatorInstance
+from openlineage.airflow.extractors import (
+    TaskMetadata,
+    BaseExtractor,
+    Extractors,
+)
+from openlineage.airflow.facets import (
+    UnknownOperatorAttributeRunFacet,
+    UnknownOperatorInstance,
+)
 from openlineage.airflow.utils import get_job_name
 
 
 class ExtractorManager:
     """Class abstracting management of custom extractors."""
+
     def __init__(self):
         self.extractors = {}
         self.task_to_extractor = Extractors()
@@ -17,23 +25,23 @@ class ExtractorManager:
         self.task_to_extractor.add_extractor(operator, extractor)
 
     def extract_metadata(
-        self,
-        dagrun,
-        task,
-        complete: bool = False,
-        task_instance=None
+        self, dagrun, task, complete: bool = False, task_instance=None
     ) -> TaskMetadata:
         extractor = self._get_extractor(task)
-        task_info = f'task_type={task.__class__.__name__} ' \
-            f'airflow_dag_id={task.dag_id} ' \
-            f'task_id={task.task_id} ' \
-            f'airflow_run_id={dagrun.run_id} '
+        task_info = (
+            f"task_type={task.__class__.__name__} "
+            f"airflow_dag_id={task.dag_id} "
+            f"task_id={task.task_id} "
+            f"airflow_run_id={dagrun.run_id} "
+        )
+        self.log.info(self)
         if extractor:
             # Extracting advanced metadata is only possible when extractor for particular operator
             # is defined. Without it, we can't extract any input or output data.
             try:
                 self.log.debug(
-                    f'Using extractor {extractor.__class__.__name__} {task_info}')
+                    f"Using extractor {extractor.__class__.__name__} {task_info}"
+                )
                 if complete:
                     task_metadata = extractor.extract_on_complete(task_instance)
                 else:
@@ -47,11 +55,10 @@ class ExtractorManager:
 
             except Exception as e:
                 self.log.exception(
-                    f'Failed to extract metadata {e} {task_info}',
+                    f"Failed to extract metadata {e} {task_info}",
                 )
         else:
-            self.log.warning(
-                f'Unable to find an extractor. {task_info}')
+            self.log.warning(f"Unable to find an extractor. {task_info}")
 
             # Only include the unkonwnSourceAttribute facet if there is no extractor
             return TaskMetadata(
@@ -62,7 +69,8 @@ class ExtractorManager:
                             UnknownOperatorInstance(
                                 name=task.__class__.__name__,
                                 properties={
-                                    attr: value for attr, value in task.__dict__.items()
+                                    attr: value
+                                    for attr, value in task.__dict__.items()
                                 },
                             )
                         ]
@@ -73,10 +81,12 @@ class ExtractorManager:
         return TaskMetadata(name=get_job_name(task))
 
     def _get_extractor(self, task) -> Optional[BaseExtractor]:
+        logging.info(self)
+        logging.info(task)
         if task.task_id in self.extractors:
             return self.extractors[task.task_id]
         extractor = self.task_to_extractor.get_extractor_class(task.__class__)
-        self.log.debug(f'extractor for {task.__class__} is {extractor}')
+        self.log.debug(f"extractor for {task.__class__} is {extractor}")
         if extractor:
             self.extractors[task.task_id] = extractor(task)
             return self.extractors[task.task_id]
